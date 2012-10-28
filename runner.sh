cc35284923646a1e9890cb6b4f1ad306721a2f83
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2012-10-28 16:26:29 +0100

    initial commit

---
+++ runner.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+EXCHANGE=/tmp/.exchange_revs
+LOCK=/tmp/.exchange.lock
+BUILD_LOG=/tmp/build_log
+MAX_RUNNERS=1
+RUN_DIR=/tmp
+
+REPO=$1
+
+if [ $# -lt 1 ];
+then
+    echo "Usage: runner.sh <Path to Repository>"
+    exit -1
+fi
+
+if [ ! -d "$REPO" ];
+then
+    echo "$REPO is not a directory"
+    exit -2
+fi
+
+while true
+do
+    touch $EXCHANGE
+    FILE_EMPTY=$(head -n1 $EXCHANGE)
+    if [ "$FILE_EMPTY" == "" ];
+    then
+        inotifywait -e modify $EXCHANGE &> /dev/null
+    fi
+
+    RUNNERS=$(ls $RUN_DIR/runner_* 2> /dev/null | wc -l)
+    if [ $RUNNERS -lt $MAX_RUNNERS ]; then
+        (
+            flock -x 200
+            REV=$(head -n1 $EXCHANGE)
+            sed -i -e "1d" $EXCHANGE
+            # set up test runner
+            (
+                RUN_FILE=$TMP/runner_$$
+                >$RUN_FILE
+                TMP=$(mktemp -d) 
+                cd $TMP
+                git clone /tmp/hooks/remote remote
+                cd remote
+                git checkout $REV
+                (make test &> /dev/null && echo "$REV builds" || \
+                    echo "$REV fails") >> $BUILD_LOG
+                cd /
+                rm -rf $TMP $RUN_FILE
+            ) &> /dev/null &
+        ) 200>> $LOCK
+    else
+        echo "No runner available, sleeping for 10 seconds"
+        sleep 10
+    fi
+done
